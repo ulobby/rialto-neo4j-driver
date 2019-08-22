3cbcf15f3088fd17c54849466f9338bf2a466d30
Simon Thordal <simonthordal@gmail.com>
2019-08-22 10:35:46 +0200

    add method signatures to client

---
+++ src/Bridge/Client.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bridge;
+
+class Client
+{
+	public function __construct(string $host, string $port)
+	{
+
+	}
+
+	public function beginTransaction(): Transaction
+	{
+
+	}
+
+	public function executeCypherQuery(): ResultSet
+	{
+
+	}
+}
