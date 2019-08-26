7c7632ca0c8236c03bedfe026fb46d0bf0aa2594
Simon Thordal <simonthordal@gmail.com>
2019-08-26 13:08:23 +0200

    Add parameter and query getters

--- src/Bridge/CypherQuery.php
+++ src/Bridge/CypherQuery.php
@@ -4,8 +4,26 @@ namespace Bridge;
 
 class CypherQuery
 {
+	private $result;
+	private $client;
+	private $query;
+	private $parameters;
+
+	public function __construct(Client $client, string $query, array $parameters)
+	{
+		$this->client = $client;
+		$this->query = $query;
+		$this->parameters = $parameters;
+	}
 	public function getResultSet(): ResultSet
 	{
+	public function getQuery()
+	{
+		return $this->query;
+	}
 
+	public function getParameters()
+	{
+		return $this->parameters;
 	}
 }
