ce7c94c78b539ee0d9ee8348363ee20ef84971cc
Simon Thordal <simonthordal@gmail.com>
2019-09-09 14:00:58 +0200

    Nodes can have multiple labels

--- src/Bridge/EntityMapper.php
+++ src/Bridge/EntityMapper.php
@@ -23,7 +23,7 @@ class EntityMapper
 	{
 		$mapping = array();
 		foreach ($columns as $column) {
-			$nodePattern = '/(?:\()' . $column. ':?[\w]*(?:\))/';
+			$nodePattern = '/(?:\()' . $column. '(:?[\w]*)+(?:\))/';
 			$relPattern = '/(?:\[)' . $column. ':?[\w]*(?:\])/';
 			$isNode = preg_match($nodePattern, $query);
 			$isRel = preg_match($relPattern, $query);
