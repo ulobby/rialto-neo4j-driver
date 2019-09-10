c1fb45b81baaa3ebf93cab7787cc55dcf4963d14
Simon Thordal <simonthordal@gmail.com>
2019-09-10 09:06:47 +0200

    Labels should be castable as String

--- src/Bridge/Label.php
+++ src/Bridge/Label.php
@@ -5,13 +5,18 @@ namespace Neo4jBridge\Bridge;
 class Label
 {
 	private $name;
-	
+
 	public function __construct($name)
 	{
 		$this->name = $name;
 	}
 
 	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function __toString()
 	{
 		return $this->name;
 	}
